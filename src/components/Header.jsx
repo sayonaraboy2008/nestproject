dc6b043ccab854d92768023b92da3cda221b8816
sayonaraboy2008 <barkamolbaxtiyarovich@gmail.com>
2026-01-24 18:20:30 +0500

    yangilandii

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,38 @@
 import React from "react";
-
+import logo from "../assets/logo.png";
 function Header() {
   return (
     <>
-      <header>
-        <img src="" alt="logo" />
-        <div>
-          <input
-            type="search"
-            placeholder="Search for products..."
-            name=""
-            id=""
-          />
-          <button>Search</button>
+      <header className="max-w-[1310px] m-auto flex justify-between items-center ">
+        <div className="flex items-center justify-between">
+          <img src={logo} alt="logo" />
+          <div>
+            <input
+              type="search"
+              placeholder="Search for products..."
+              name=""
+              id=""
+            />
+            <button>Search</button>
+          </div>
         </div>
+        <ul className="flex items-center gap-6">
+          <div>
+            <h3>Became Vendor</h3>
+          </div>
+          <li>
+            <a href="">Compare</a>
+          </li>
+          <li>
+            <a href="">Wishlist</a>
+          </li>
+          <li>
+            <a href="">Cart</a>
+          </li>
+          <li>
+            <a href="">Account</a>
+          </li>
+        </ul>
       </header>
     </>
   );
